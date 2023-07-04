d5775e4d57476673665fd7abe8f75ec12872d87c
Moses Mwangi <moses.soft.eng@gmail.com>
2023-07-04 14:50:11 +0300

    Add icon button
    Co-Authored-By: Charity Sammy <99351794+Charity012@users.noreply.github.com>
    Co-Authored-By: xngigez <7607027+xngigez@users.noreply.github.com>

--- app/src/main/java/ai/chat/x/ai/images/presentation/components/ChatBox.kt
+++ app/src/main/java/ai/chat/x/ai/images/presentation/components/ChatBox.kt
@@ -1,5 +1,6 @@
 package ai.chat.x.ai.images.presentation.components
 
+import ai.chat.x.ai.images.R
 import ai.chat.x.ai.images.data.model.Message
 import ai.chat.x.ai.images.data.repository.getAIResponse
 import androidx.compose.foundation.background
@@ -9,10 +10,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -23,12 +31,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
@@ -43,21 +55,26 @@ fun ChatBox(chatGptApiKey: String, chatList: MutableList<Message>) {
     ) {
         Row(
             modifier = Modifier
+                .fillMaxWidth()
                 .background(Color.LightGray)
                 .align(Alignment.BottomCenter)
-                .height(IntrinsicSize.Min)
-                .width(IntrinsicSize.Max)
+                .height(IntrinsicSize.Min),
+            verticalAlignment = Alignment.Bottom
         ) {
             TextField(
-
                 value = myVar,
                 onValueChange = { newText ->
                     myVar = newText
                 },
+                modifier = Modifier
+                    .weight(1f)
+
+                    .wrapContentHeight(),
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
                 visualTransformation = VisualTransformation.None
             )
-            Button(
+
+            IconButton(
                 onClick = {
                     softwareKeyboardController?.hide()
 
@@ -67,9 +84,13 @@ fun ChatBox(chatGptApiKey: String, chatList: MutableList<Message>) {
                     getAIResponse(chatGptApiKey, chatList)
 
                     myVar = TextFieldValue("")
-                }
+                },
+                modifier = Modifier.size(48.dp),
             ) {
-                Text(text = "Send")
+                Icon(
+                    painter = painterResource(R.drawable.send_fill0_wght400_grad0_opsz48),
+                    contentDescription = "Send"
+                )
             }
         }
     }
