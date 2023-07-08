81a7a577f34e67086986bc47d44f6293ea0df398
Moses Mwangi <moses.soft.eng@gmail.com>
2023-07-08 22:05:41 +0300

    Add function to truncate string
    Co-Authored-By: Charity Sammy <99351794+Charity012@users.noreply.github.com>
    Co-Authored-By: xngigez <7607027+xngigez@users.noreply.github.com>

--- app/src/main/java/ai/chat/x/ai/images/domain/StringHelper.kt
+++ app/src/main/java/ai/chat/x/ai/images/domain/StringHelper.kt
@@ -1,4 +1,11 @@
 package ai.chat.x.ai.images.domain
 
 object StringHelper {
+    fun getTruncatedString(input: String, maxLength: Int): String {
+        return if (input.length > maxLength) {
+            input.substring(0, maxLength)
+        } else {
+            input
+        }
+    }
 }
