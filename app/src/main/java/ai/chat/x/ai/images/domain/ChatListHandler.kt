91efd2d63b320451e87fa359e607b88ec2998cb4
Moses Mwangi <moses.soft.eng@gmail.com>
2023-07-08 11:09:13 +0300

    Update ChatListHandler.kt
    Co-Authored-By: Charity Sammy <99351794+Charity012@users.noreply.github.com>

--- app/src/main/java/ai/chat/x/ai/images/domain/ChatListHandler.kt
+++ app/src/main/java/ai/chat/x/ai/images/domain/ChatListHandler.kt
@@ -1,9 +1,14 @@
 package ai.chat.x.ai.images.domain
 
 import ai.chat.x.ai.images.data.model.ChatItem
+import com.google.gson.Gson
 
 object ChatListHandler {
     fun getLatestMessage(chatItemList: List<ChatItem>): ChatItem? {
         return chatItemList.lastOrNull()
+    }
+
+    fun getChatItemListJsonString(chatItemList: List<ChatItem>): String {
+        return Gson().toJson(chatItemList)
     }
 }
